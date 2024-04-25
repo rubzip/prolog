4e7eaab6d1562bb66d57e997abfba97d42a8a2ec
Rubén <rubgij@gmail.com>
2024-04-25 13:02:08 +0200

    todos hechos algunos mal

--- prolog/ex1.pl
+++ prolog/ex1.pl
@@ -34,3 +34,72 @@ s(Y,_,Word2,_,_,_),
 ant(X,_,Z,_),
 sim(Y,Z)
 ).
+% 3
+not_repeated(List):-
+length(List, N),
+list_to_set(List, Set),
+length(Set, M),
+N==M.
+
+all_direct_antonyms_offset_offset(Offset, List_of_Offsets) :-
+    findall(X, ant(Offset, _, X, _), Antonyms),
+    not_repeated(List_of_Offsets),
+    subset(Antonyms, List_of_Offsets).
+
+% Exercise 4
+all_direct_antonyms_word_offset(Word, List_of_Offsets) :-
+    s(Offset,_,Word,_,_,_),
+    findall(X, ant(Offset, _, X, _), Antonyms),
+    not_repeated(List_of_Offsets),
+    subset(Antonyms, List_of_Offsets).
+
+% Exercise 5
+all_direct_antonyms_offset_word(Offset, List_of_Words) :-
+    findall(X, ant(Offset, _, X, _), Antonyms),
+    findall(Y, (s(Y,_,Word,_,_,_),member(Word,List_of_Words)), List_of_Offsets),
+    not_repeated(List_of_Offsets),
+    subset(Antonyms, List_of_Offsets).
+
+% Exercise 6
+all_direct_antonyms_word_word(Word, List_of_Words) :-
+    s(Offset,_,Word,_,_,_),
+    findall(X, ant(Offset, _, X, _), Antonyms),
+    findall(Y, (s(Y,_,WordFromList,_,_,_),member(WordFromList,List_of_Words)), List_of_Offsets),
+    not_repeated(List_of_Offsets),
+    subset(Antonyms, List_of_Offsets).
+
+
+% Exercise 7
+is_antonym_via_similarity(Offset1, Offset2):-
+    (ant(Offset1,_,OffsetAux,_),
+    sim(OffsetAux,Offset2));
+    (ant(Offset2,_,OffsetAux,_),
+    sim(OffsetAux,Offset1)).
+
+is_antonym_offset(Offset1, Offset2):-
+    ant(Offset1,_,Offset2,_),
+    is_antonym_via_similarity(Offset1, Offset2).
+
+all_antonyms_offset(Offset,List_of_Offsets):-
+    findall(X, is_antonym_offset(Offset, X), Antonyms),
+    not_repeated(List_of_Offsets),
+    subset(Antonyms, List_of_Offsets).
+%all_antonyms_offset(300013887, [300016756,300017024]).
+
+% Exercise 8    Termnar **
+all_antonyms_word(Word,List_of_Words):-
+    findall(X, is_antonym(Word, X), Antonyms),
+    not_repeated(List_of_Words),
+    subset(Antonyms, List_of_Words).
+
+% Exercise 9
+number_of_antonyms_offset(Offset,Count):-
+    findall(X, is_antonym_offset(Offset, X), Antonyms),
+    Count==length(Antonyms).
+
+% Exercise 10
+number_of_antonyms_word(Word,Count):-
+    s(Offset,_,Word,_,_,_),
+    findall(X, is_antonym_offset(Offset, X), Antonyms),
+    Count==length(Antonyms).
+
